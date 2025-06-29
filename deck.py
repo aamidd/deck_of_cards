70e1263fa98fb3b5c8fa0b0060f9501cea4f2a63
amid <sanaiamid@gmail.com>
2025-06-29 21:42:12 +0330

    add Deck

---
+++ deck.py
@@ -0,0 +1,45 @@
+from random import randint
+
+
+class Deck:
+    def __init__(self):
+        self.ranks = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"]
+        self.suits = ["clubs", "spades", "diamonds", "hearts"]
+        self.cards = [(rank, suit) for rank in self.ranks for suit in self.suits]
+
+    # print the deck
+    def print_deck(self):
+        for rank, suit in self.cards:
+            print(f"{rank} of {suit}")
+
+    # shuffle the deck
+    def shuffle(self):
+        for i in range(52):
+            random_place = randint(0, 51)
+            tmp = self.cards[i]
+            self.cards[i] = self.cards[random_place]
+            self.cards[random_place] = tmp
+
+    # peek the first card on the deck
+    def peek_first(self):
+        return self.cards[0]
+
+    # peek the last card on the deck
+    def peek_last(self):
+        return self.cards[-1]
+
+    # deal the first card on the deck
+    def deal_first(self):
+        return self.cards.pop(0)
+
+    # deal the last card on the deck
+    def deal_last(self):
+        return self.cards.pop(-1)
+
+    # deal the specified number of cards
+    def deal(self, number_of_cards=1):
+        dealt_cards = []
+        for i in range(number_of_cards):
+            dealt_cards.append(self.deal_first())
+
+        return dealt_cards
